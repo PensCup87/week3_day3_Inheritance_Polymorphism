b1819d6c76be64403ffecaa76e9f827fd228ab47
PensCup87 <bdt9123@gmail.com>
2017-10-11 11:59:42 -0400

    Added a Child class automobile

---
+++ week3_day3_Inheritance_Polymorphism/Automobile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_day3_Inheritance_Polymorphism
+{
+    class Automobile : vehicle
+    {
+        //want this class to inherit from BaseVehicle with : vehicle
+    }
+}
